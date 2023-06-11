ecbc7929c57233f8d133b08dfd09cc672042029d
Hajungwon01 <86599321+Hajungwon01@users.noreply.github.com>
2023-06-11 18:34:28 +0900

    Add files via upload

---
+++ src/main/java/com/example/webPOS/dao/interfaces/InventoryDAO.java
@@ -0,0 +1,9 @@
+package com.example.webPOS.dao.interfaces;
+
+import java.util.List;
+
+import com.example.webPOS.dto.Inventory;
+
+public interface InventoryDAO {
+    List<Inventory> findAll(String storeName);
+}

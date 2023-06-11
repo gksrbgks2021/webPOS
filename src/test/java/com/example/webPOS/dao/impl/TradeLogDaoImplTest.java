1ea00ed1b6c8320f9abe088df250bd741d500695
gksrbgks2021 <gksrbgks2021@gmail.com>
2023-06-11 14:45:01 +0900

    add new test

---
+++ src/test/java/com/example/webPOS/dao/impl/TradeLogDaoImplTest.java
@@ -0,0 +1,24 @@
+package com.example.webPOS.dao.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TradeLogDaoImplTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}

6ac1e4fbe6d785d7b4dc721ab01e595269463bed
Hajungwon01 <86599321+Hajungwon01@users.noreply.github.com>
2023-06-11 21:19:23 +0900

    Add files via upload

--- src/main/java/com/example/webPOS/dao/interfaces/InventoryDAO.java
+++ src/main/java/com/example/webPOS/dao/interfaces/InventoryDAO.java
@@ -5,6 +5,11 @@ import java.util.List;
 import com.example.webPOS.dto.Inventory;
 
 public interface InventoryDAO {
-    List<Inventory> findAll(String storeName);
-    void update(Inventory inventory);
+	List<Inventory> findAll(String storeName);
+
+	int getQuantityByProductId(String productID, String storeName);
+
+	void update(String productID, int quantity, String storeName, boolean option); // false (-) true (+)
+
+	void insert(String productID, int quantity, String storeName);
 }

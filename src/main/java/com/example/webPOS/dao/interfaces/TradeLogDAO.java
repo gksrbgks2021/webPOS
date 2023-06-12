01a9b1c4d0d5b0b2e7be4bd5a662ef1e6228e890
Hajungwon01 <86599321+Hajungwon01@users.noreply.github.com>
2023-06-13 07:07:58 +0900

    Add files via upload

--- src/main/java/com/example/webPOS/dao/interfaces/TradeLogDAO.java
+++ src/main/java/com/example/webPOS/dao/interfaces/TradeLogDAO.java
@@ -3,11 +3,13 @@ package com.example.webPOS.dao.interfaces;
 import com.example.webPOS.vo.TradeLog;
 
 import java.util.List;
+import java.util.Map;
 
 public interface TradeLogDAO {
     void save(TradeLog tradeLog);
-    List<TradeLog> findPeriod(String start, String end, String StoreName);
-    List<TradeLog> findPeriodDesc(String start, String end, String dateType);//기간 조회
+    Map<String, Integer> getProfit(String start, String end, String StoreName, String period);
+    Map<String, String> profitFirst(String start, String end, String period);
+    Map<String, String> takeFirst(String start, String end, String period);
 
     TradeLog findById(Long productID);
 }
